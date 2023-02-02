22efe07bb0c35486f70cbcf2a076d8de78971c4a
sua <tigertigertigersuasua@gmail.com>
2023-02-02 16:44:45 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Home
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path='about' element={<About />} />
+            <Route path='conatct' element={<Contact />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { NavLink ,Link } from 'react-router-dom'
+
+function Header() {
+  return (
+    <>
+      <header className='header-topstrip-strip py-3'>
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <p>Free Shipping Over $100 & Free Return</p>
+            </div>
+            <div className="col-6">
+              
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default Header
